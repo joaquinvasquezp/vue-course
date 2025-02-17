72b7a0dbcbc1ae01402faebef578cc0d17fb11ce
Joaquín Vásquez <joaquin.vasp@gmail.com>
2025-02-17 15:07:52 -0300

    Data binding

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,3 +1,21 @@
-const app = Vue.createApp();
+const app = Vue.createApp({
+    data() {
+        return {
+            courseGoalA: '<h1>Finish the course and learn Vue!</h1>',
+            courseGoalB: '<i>Master Vue and build amazing apps!</i>',
+            vueLink: 'https://vuejs.org/'
+        }
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return this.courseGoalA;
+            } else {
+                return this.courseGoalB;
+            }
+        }
+    }
+});
 
 app.mount('#user-goal');
