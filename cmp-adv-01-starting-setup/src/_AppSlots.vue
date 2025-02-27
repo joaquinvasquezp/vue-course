75773fb2f3987c24ad1c7c22750d0741e7b5fa9e
Joaquín Vásquez <joaquin.vasp@gmail.com>
2025-02-27 11:54:48 -0300

    Dynamic components

---
+++ cmp-adv-01-starting-setup/src/_AppSlots.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <!--<TheHeader /> supported, if you past it like the comment component-->
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps['another-prop'] }}</p>
+      </template>
+    </course-goals>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+import UserInfo from './components/UserInfo.vue';
+import BadgeList from './components/BadgeList.vue';
+import CourseGoals from './components/CourseGoals.vue';
+
+
+export default {
+  components: {
+    TheHeader,
+    // 'the-header': TheHeader -> also supported
+    UserInfo,
+    BadgeList,
+    CourseGoals
+  },
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
