0127ab9da5c272988bb4a84a6531c78279b0ed23
Joaquín Vásquez <joaquin.vasp@gmail.com>
2025-02-26 23:37:51 -0300

    Slots

---
+++ cmp-adv-01-starting-setup/src/components/CourseGoals.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <li v-for="goal in goals" :key="goal">
+            <slot :item="goal" another-prop="..."></slot> <!-- props on slots, accesable in the component where you pass data to the slot -->
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            goals: ['Finish the course', 'Learn Vue'],
+        }
+    }
+}
+</script>
