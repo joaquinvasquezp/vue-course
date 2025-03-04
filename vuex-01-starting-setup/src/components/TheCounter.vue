c50ab4f5760470d1023cd13bcccff6d4bde3d9f8
Joaquín Vásquez <joaquin.vasp@gmail.com>
2025-03-03 21:47:47 -0300

    Vuex

---
+++ vuex-01-starting-setup/src/components/TheCounter.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>"TheCounter" component</p>
+    <h3>{{ counter }}</h3>
+</template>
+
+<script>
+export default {
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    }
+  }
+};
+</script>
